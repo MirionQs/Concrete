055c30e865334b5af1aeefbd6a6d9a642aa1ed65
Mirion <51433180+MirionQs@users.noreply.github.com>
2024-05-24 01:27:37 +0800

    add fenwick_tree impl

---
+++ fenwick_tree.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <functional>
+
+namespace concrete {
+
+	// point update and range query
+	template <class T, class binary_operator = ::std::plus<>>
+	class fenwick_tree_pr : private ::std::vector<T> {
+		using base = ::std::vector<T>;
+
+		static constexpr binary_operator _op{};
+
+		T& _at(size_t index) noexcept {
+			return base::operator[](index);
+		}
+
+	public:
+		explicit fenwick_tree_pr(size_t size) noexcept : base(size) {}
+
+		template <::std::input_iterator iter>
+		explicit fenwick_tree_pr(iter first, iter last) noexcept : base(first, last) {
+			size_t size{this->size()};
+			for (size_t i{0}; i != size; ++i) {
+				size_t j{i | i + 1};
+				if (j < size) {
+					_at(j) = _op(_at(j), _at(i));
+				}
+			}
+		}
+
+		void apply(size_t index, const T& value) noexcept {
+			size_t size{this->size()};
+			while (index < size) {
+				_at(index) = _op(_at(index), value);
+				index |= index + 1;
+			}
+		}
+
+		T operator[](size_t index) noexcept {
+			T res{};
+			size_t size{this->size()};
+			while (index < size) {
+				res = _op(res, _at(index));
+				index &= index + 1;
+				--index;
+			}
+			return res;
+		}
+	};
+
+	// range update and point query
+	template <class T, class binary_operator = ::std::plus<>, class inverse_binary_operator = ::std::minus<>, class inverse_operator = ::std::negate<>>
+	class fenwick_tree_rp : private fenwick_tree_pr<T, binary_operator> {
+		using base = fenwick_tree_pr<T, binary_operator>;
+
+		static constexpr inverse_binary_operator _invOp{};
+		static constexpr inverse_operator _inv{};
+
+		template <::std::input_iterator iter>
+		static base _construct(iter first, iter last) {
+			::std::vector<T> diff(first, last);
+			for (size_t i{diff.size() - 1}; i != 0; --i) {
+				diff[i] = _invOp(diff[i], diff[i - 1]);
+			}
+			return base{diff.begin(), diff.end()};
+		}
+
+	public:
+		using base::operator[];
+
+		explicit fenwick_tree_rp(size_t size) noexcept : base{size} {}
+
+		template <::std::input_iterator iter>
+		explicit fenwick_tree_rp(iter first, iter last) noexcept : base{::std::move(_construct(first, last))} {}
+
+		void apply(size_t first, size_t last, const T& value) noexcept {
+			base::apply(first, value);
+			base::apply(last, _inv(value));
+		}
+	};
+}
